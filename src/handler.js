ab9430d5cb9c199d124ea6f4c06c588155ec4b6b
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-02-09 22:37:01 +0700

    Add files via upload

---
+++ src/handler.js
@@ -0,0 +1,68 @@
+const { nanoid } = require('nanoid')
+const notes = require('./notes')
+
+const addNoteHandler = (request, h) => {
+  const { title, tag, body } = request.payload
+
+  const id = nanoid(16)
+  const createdAT = new Date().toISOString()
+  const updatedAT = createdAT
+
+  const newNote = {
+    id, title, tag, body, createdAT, updatedAT
+  }
+
+  notes.push(newNote)
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0
+
+  if (isSuccess) {
+    const response = h.response(
+      {
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
+        data: {
+          noteId: id
+        }
+      }
+    )
+    response.code(201)
+    return response
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan'
+  })
+  response.code(500)
+  return response
+}
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes
+  }
+})
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const note = notes.filter((n) => n.id === id)[0]
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note
+      }
+    }
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler }
